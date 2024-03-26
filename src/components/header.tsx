7b6b5bcba7d947cb9999593adb0aa5fa4d614cc7
Justin Richter <Justin.Richter@SimPlan.de>
2024-03-26 15:19:33 +0100

    Update timeEntryTable.tsx with DevExtreme Data Grid and add data source; update timeEntryTable.css with unnecessary rules removed
    This commit updates the TimeEntryTable component in timeEntryTable.tsx by replacing the existing table with a DevExtreme Data Grid component and adding a data source using the ODataStore. Additionally, unnecessary rules have been removed from timeEntryTable.css.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,70 @@
-import { Logo } from "./Logo";
-import { User } from "./User";
+import React, { useState } from "react";
+import { DropDownButton } from "devextreme-react";
 import "./header.css";
+import { Logo } from "./Logo";
 
 export function Header() {
+  const [dropdownValue, setDropdownValue] = useState("January");
+
+  const dropdownItems = [
+    { id: 1, text: "January" },
+    { id: 2, text: "February" },
+    { id: 3, text: "March" },
+    { id: 4, text: "April" },
+    { id: 5, text: "May" },
+    { id: 6, text: "June" },
+    { id: 7, text: "July" },
+    { id: 8, text: "August" },
+    { id: 9, text: "September" },
+    { id: 10, text: "October" },
+    { id: 11, text: "November" },
+    { id: 12, text: "December" },
+  ];
+
+  const handleDropdownValueChanged = (e: any) => {
+    setDropdownValue(e.itemData.text);
+  };
+
   return (
     <>
       <div className="navbar">
         <Logo />
         <ul className="navlist">
           <li>
-            <a href="#hallo">Tabellenansicht</a>
+            <a href="#hallo">Notizen</a>
           </li>
           <li>
-            <a href="#tschüss">Notizen</a>
+            <div className="dropdown-button">
+              <DropDownButton
+                dataSource={dropdownItems}
+                valueExpr="text"
+                displayExpr="text"
+                value={dropdownValue}
+                onValueChanged={handleDropdownValueChanged}
+                width="160px"
+              >
+                <div className="headerUser">
+                  <span>User</span>
+                  <svg
+                    width="10"
+                    height="6"
+                    viewBox="0 0 10 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M1.06006 5.22222L4.99993 1.77778L9.03989 5.22222"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+              </DropDownButton>
+            </div>
           </li>
         </ul>
-
-        <User />
       </div>
     </>
   );
--- src/components/timeEntryTable.tsx
+++ src/components/timeEntryTable.tsx
@@ -1,45 +1,20 @@
-import React from "react";
+import DataGrid from "devextreme-react/data-grid";
+import { customers } from "./data";
 import "./timeEntryTable.css";
 
-export const TimeEntryTable = () => {
-  //funktionen der Tabelle hier
-  return (
-    <>
-      <div className="table-container">
-        <div className="timetable">
-          <table>
-            <thead>
-              <tr>
-                <th>Vergangene Zeit</th>
-                <th>Uhrzeit </th>
-                <th>Projekt </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>8h 30min</td>
-                <td>12:50 Uhr</td>
-                <td>Projekt 2b</td>
-              </tr>
-              <tr>
-                <td>8h 30min</td>
-                <td>18:45 Uhr</td>
-                <td>Projekt 3</td>
-              </tr>
-              <tr>
-                <td>8h 30min</td>
-                <td>18:45 Uhr</td>
-                <td>Projekt 3</td>
-              </tr>
-              <tr>
-                <td>8h 30min</td>
-                <td>18:45 Uhr</td>
-                <td>Projekt 3</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </>
-  );
-};
+const columns = [
+  "Vergangene Zeit",
+  "Start Uhrzeit",
+  "End Uhrzeit",
+  "Projekt",
+  "Notizen(#)",
+];
+
+export const TimeEntryTable = () => (
+  <DataGrid
+    dataSource={customers}
+    keyExpr="ID"
+    defaultColumns={columns}
+    showBorders={true}
+  />
+);
