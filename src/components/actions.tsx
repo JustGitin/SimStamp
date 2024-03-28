1e1d9a2d72e275b0f172ba4c995cf32e57955656
Justin Richter <Justin.Richter@SimPlan.de>
2024-03-28 14:34:27 +0100

    Insert new entries into the table //interim status

--- src/components/actions.tsx
+++ src/components/actions.tsx
@@ -3,27 +3,78 @@ import "./actions.css";
 import { StopButton } from "./stopButton.tsx";
 import { StartButton } from "./startButton.tsx";
 import { ResetButton } from "./resetButton.tsx";
-import { DateTime } from "luxon";
+import { DateTime, Duration } from "luxon";
 import { timeEntries } from "./data.ts";
-import { DataGrid } from "devextreme-react";
 
 export const Actions = () => {
+  const notes = "Hallo, ich bin eine Notiz";
+  const projectName = "SimStamp";
+
   const [isRunning, setIsRunning] = useState(false);
   const [startStamp, setStartStamp] = useState<DateTime | null>(null);
+  const [stopStamp, setStopStamp] = useState<DateTime | null>(null);
+  const [elapsedTime, setElapsedTime] = useState<Duration>(
+    Duration.fromMillis(0)
+  );
+
+  const formatStamps = (varToFormat: DateTime): string => {
+    return varToFormat.toFormat("HH:mm:ss");
+  };
+
+  function formatDuration(elapsedTime: Duration): string {
+    return `${Math.floor(elapsedTime.as("hours"))}:${elapsedTime.toFormat(
+      "mm:ss"
+    )}`;
+  }
+  //Die idee ist ein Array mit allen Parametern welches mit einer forEach durchlaufen wird und jeden Eintrag in einen String formatiert,bevor der neue Eintrag erstellt wird
+  //ideal wäre das ausgrenzen der Strings um keine redundante Formatierung zu machen
+
+  const calculateDifference = (stopStamp: DateTime, startStamp: DateTime) => {
+    const difference = stopStamp.diff(startStamp);
+    setElapsedTime(difference);
+  };
+  const createNewEntry = (
+    startStamp: DateTime,
+    stopStamp: DateTime,
+    projectName: string,
+    notes: string
+  ) => {
+    const now = DateTime.now(); //currentDate ist das Datum zum Zeitpunkt des Eintrags
+    const currentDate: string = now.toISODate();
+    const newEntryID = timeEntries[timeEntries.length - 1].ID + 1;
+
+    let newTimeEntry = {
+      ID: newEntryID,
+      Datum: currentDate,
+      VergangeneZeit: formatDuration(elapsedTime),
+      StartUhrzeit: formatStamps(startStamp),
+      EndUhrzeit: formatStamps(stopStamp),
+      Projekt: projectName,
+      Notizen: notes,
+    };
+    timeEntries.push(newTimeEntry);
+  };
 
   return (
     <>
       <div className="button-container">
         <StopButton
           onStop={() => {
-            setIsRunning(false);
-            const newEntryID = timeEntries[timeEntries.length - 1].ID + 1; //jetzt neuer Eintrag mit der newEntryID
+            if (startStamp) {
+              setIsRunning(false);
+              const now = DateTime.now();
+              setStopStamp(now);
+              calculateDifference(now, startStamp);
+              createNewEntry(startStamp, stopStamp!, projectName, notes); //ACHTUNG SCHUMMLEREI MIT !
+            } else {
+              console.log("startStamp is not defined");
+            }
           }}
         />
         <StartButton
           onStart={() => {
             setIsRunning(true);
-            setStartStamp(DateTime.now()); //brauche eigentlich nur die Zeit und nicht alles
+            setStartStamp(DateTime.now());
           }}
         />
         <ResetButton
